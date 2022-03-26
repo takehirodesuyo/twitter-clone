f61579c79fe93e85182a44655184ed254c78439c
岸田岳大 <kishidatakehiro@Hajimari-TakehiroKishida.local>
2022-03-26 20:21:21 +0900

    フォロー機能 view追加

--- app/resources/views/users/index.blade.php
+++ app/resources/views/users/index.blade.php
@@ -7,10 +7,30 @@
                 @foreach ($all_users as $user)
                     <div class="card">
                         <div class="card-haeder p-3 w-100 d-flex">
-                            <img src="{{ $user->profile_image }}" class="rounded-circle" width="50" height="50">
                             <div class="ml-2 d-flex flex-column">
                                 <p class="mb-0">{{ $user->name }}</p>
-                                <a href="{{ url('users/' .$user->id) }}" class="text-secondary">{{ $user->screen_name }}</a>
+                                <a href="{{ url('users/' .$user->id) }}" class="text-secondary">プロフィール</a>
+                            </div>
+                            @if (auth()->user()->isFollowed($user->id))
+                                <div class="px-2">
+                                    <span class="px-1 bg-secondary text-light">フォローされています</span>
+                                </div>
+                            @endif
+                            <div class="d-flex justify-content-end flex-grow-1">
+                                @if (auth()->user()->isFollowing($user->id))
+                                    <form action="{{ route('unfollow', $user->id ) }}" method="POST">
+                                        {{ csrf_field() }}
+                                        {{ method_field('DELETE') }}
+
+                                        <button type="submit" class="btn btn-danger">フォロー解除</button>
+                                    </form>
+                                @else
+                                    <form action="{{ route('follow', $user->id ) }}" method="POST">
+                                        {{ csrf_field() }}
+
+                                        <button type="submit" class="btn btn-primary">フォローする</button>
+                                    </form>
+                                @endif
                             </div>
                         </div>
                     </div>
